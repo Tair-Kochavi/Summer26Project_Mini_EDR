1529a90ec46e0d3ffd627159b03dcf4805407d07
Tair Kochavi <tair.kochavi@campus.technion.ac.il>
2026-09-15 18:26:13 +0300

    finish run_target implementation and basic trace_target

--- main.cpp
+++ main.cpp
@@ -12,31 +12,34 @@ pid_t trace_target(pid_t pid) {
     int status;
     struct user_regs_struct regs;
 
-    while (!WEIFEXITED(status)) {
+    wait(&status);
+    while (!WIFEXITED(status)) {
         //wait for the program to use a syscall and then print the syscall number
         ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
         wait(&status);
-        ptrace(PTRACE_GETREGS, pid, NULL, &regs);
-        std::cout << "syscall used: " << regs->orig_rax << "\n";
+        if (!WIFEXITED(status)) {
+            ptrace(PTRACE_GETREGS, pid, NULL, &regs);
+            std::cout << "syscall used: " << regs.orig_rax << "\n";
+        }
         ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
         wait(&status);
-
-
-        wait(&status)
     }
+
+    return status;
 }
 
-pid_t run_target(const char* path) {
+pid_t run_target(char* args[]) {
     pid_t pid = fork();
     if (pid == 0) {
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);
-        execve(path, , );
+        execve(args[0], args, NULL);
     }
     if (pid > 0) {
-        trace_target(pid);
+        return trace_target(pid);
     }
     else {
         std::cerr << "fork failed\n";
+        return -1;
     }
 }
 
@@ -44,5 +47,9 @@ pid_t run_target(const char* path) {
 
 
 int main(int argc, char* argv[]) {
-    run_target();
+    char **args_for_exec = argv + 1;
+    if (run_target(args_for_exec) == -1) {
+        return -1;
+    }
+    return 0;
 }
