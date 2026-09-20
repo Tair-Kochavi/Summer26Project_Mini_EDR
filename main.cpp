220b3c59185dd1ed510592b3574b110c1d64f267
Tair Kochavi <tair.kochavi@campus.technion.ac.il>
2026-09-20 10:06:44 +0300

    add basic handle syscall function

--- main.cpp
+++ main.cpp
@@ -8,6 +8,19 @@
 #include <sys/syscall.h>
 
 
+
+void handle_syscall(const user_regs_struct& regs) {
+    switch (regs.orig_rax) {
+        case 2:  //open syscall
+            std::cout << "memory address of path: " << regs.rdi <<"\n";
+            break;
+        case 59: //execve syscall
+            std::cout << "memory address of path: " << regs.rdi <<"\n";
+            break;
+    }
+}
+
+
 pid_t trace_target(pid_t pid) {
     int status;
     struct user_regs_struct regs;
@@ -20,6 +33,7 @@ pid_t trace_target(pid_t pid) {
         if (!WIFEXITED(status)) {
             ptrace(PTRACE_GETREGS, pid, NULL, &regs);
             std::cout << "syscall used: " << regs.orig_rax << "\n";
+            handle_syscall(regs);
         }
         ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
         wait(&status);
@@ -32,7 +46,9 @@ pid_t run_target(char* args[]) {
     pid_t pid = fork();
     if (pid == 0) {
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);
-        execve(args[0], args, NULL);
+        if (execve(args[0], args, NULL) == -1) {
+            exit(1);
+        }
     }
     if (pid > 0) {
         return trace_target(pid);
