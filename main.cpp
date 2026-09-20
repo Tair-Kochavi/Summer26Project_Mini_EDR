8ab2de455d0ca3a87489177ef4eea1b2b7dc26c5
Tair Kochavi <tair.kochavi@campus.technion.ac.il>
2026-09-20 16:12:00 +0300

    start get_path implementation

--- main.cpp
+++ main.cpp
@@ -6,10 +6,15 @@
 #include <sys/wait.h>
 #include <sys/user.h>
 #include <sys/syscall.h>
+#define PATH_MAX 4096
 
 
-
-void handle_syscall(const user_regs_struct& regs) {
+/**
+ *
+ * @param regs register values of the child process after it called a syscall
+ * @param pid the pid of the child process
+ */
+void handle_syscall(const user_regs_struct& regs, pid_t pid) {
     switch (regs.orig_rax) {
         case 2:  //open syscall
             std::cout << "memory address of path: " << regs.rdi <<"\n";
@@ -24,6 +29,41 @@ void handle_syscall(const user_regs_struct& regs) {
 }
 
 
+
+
+/**
+ *
+ * @param pid child process pid
+ * @param addr address of the path to the file the child process wants to use
+ * @return a string of the content in addr (path to the file)
+ */
+std::string get_path(pid_t pid, unsigned long long addr) {
+    int bytes = 0;
+    std::string path = "";
+    long to_append = 0;
+    while (bytes < PATH_MAX) {
+        long res = ptrace(PTRACE_PEEKDATA, pid, addr, NULL);
+        bytes += 8;
+        for (int i = 0; i < 8; i++) {
+            long mask = 0x0000000F;
+            to_append = res & mask;
+            if (to_append == 0) {
+                break;
+            }
+            path += std::to_string(to_append);
+
+            res = res >> 8;
+        }
+    }
+    if (to_append != 0) {  //not null terminated - path too long
+
+    }
+    return path;
+}
+
+
+
+
 pid_t trace_target(pid_t pid) {
     int status;
     struct user_regs_struct regs;
@@ -36,7 +76,7 @@ pid_t trace_target(pid_t pid) {
         if (!WIFEXITED(status)) {
             ptrace(PTRACE_GETREGS, pid, NULL, &regs);
             std::cout << "syscall used: " << regs.orig_rax << "\n";
-            handle_syscall(regs);
+            handle_syscall(regs, pid);
         }
         ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
         wait(&status);
@@ -44,6 +84,8 @@ pid_t trace_target(pid_t pid) {
 
     return status;
 }
+
+
 
 pid_t run_target(char* args[]) {
     pid_t pid = fork();
