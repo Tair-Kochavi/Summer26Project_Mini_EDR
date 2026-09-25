b391c18124f5c5261b0924aff1aa991c7fbb6cb5
Tair Kochavi <tair.kochavi@campus.technion.ac.il>
2026-09-25 18:40:56 +0300

    add syscall_info struct, change return type of get_syscall_name and start log_syscall implementation

--- syscall_format.cpp
+++ syscall_format.cpp
@@ -1,16 +1,17 @@
 
 #include "syscall_format.h"
 
+#include <iostream>
 #include <unordered_map>
 
 namespace {
-    const std::unordered_map<int, std::string> sys_map = {
-        {0, "read"},
-        {1, "write"},
-        {2, "open"},
-        {3, "close"},
-        {59, "execve"},
-        {257, "openat"}
+    const std::unordered_map<int, std::pair<std::string, std::string>> sys_map = {
+        {0, {"read", "data read"}},
+        {1, {"write", "data output"}},
+        {2, {"open", "file access"}},
+        {3, {"close", "resource release"}},
+        {59, {"execve", "process execution"}},
+        {257, {"openat", "file access"}}
     };
 }
 
@@ -19,10 +20,16 @@ namespace {
  * @param num the syscall number
  * @return string explaining which syscall was used
  */
-std::string get_syscall_name(const int num) {
+std::pair<std::string, std::string> get_syscall_name(const int num) {
     auto it = sys_map.find(num);
     if (it != sys_map.end()) {  //syscall number found in sys_map
         return it->second;
     }
-    return "SYS_" + std::to_string(num);
+    return {"SYS_" + std::to_string(num), ""};
+}
+
+
+void log_syscall(const syscall_info info) {
+    std::pair<std::string, std::string> sys_info = get_syscall_name(info.sys_num);
+
 }
--- syscall_format.h
+++ syscall_format.h
@@ -2,4 +2,11 @@
 #pragma once
 #include <string>
 
-std::string get_syscall_name(const int num);
+
+struct syscall_info {
+    int sys_num;
+    std::string path;
+};
+
+std::pair<std::string, std::string> get_syscall_name(const int num);
+void log_syscall(const syscall_info info);
